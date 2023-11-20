16686f6b1df22bbc794abd6586ecb9e3e16507f0
sbaz94collect <s.baz@santmarc.com>
2023-11-20 20:02:34 +0100

    Beta

--- Gatos/Assets/Scripts/Abrir_puerta.cs
+++ Gatos/Assets/Scripts/Abrir_puerta.cs
@@ -5,15 +5,6 @@ using UnityEngine;
 public class Abrir_puerta : MonoBehaviour
 {
 
-    private void OnTriggerEnter(Collider other)
-    {
-        if (other.gameObject.CompareTag("Client"))
-        {
-            Debug.Log("hola");
-            Puerta.Instance._animator_puerta.SetTrigger("Abrir_puerta");
-        }
-        
-    }
 
 
 }
--- Gatos/Assets/Scripts/Puerta.cs
+++ Gatos/Assets/Scripts/Puerta.cs
@@ -5,21 +5,10 @@ using UnityEngine;
 
 public class Puerta : MonoBehaviour
 {
-    private static Puerta instance;
-    public static Puerta Instance => instance;
+    
     [SerializeField] public Animator _animator_puerta;
     
-    private void Awake()
-    {
-        if (instance == null)
-        {
-            instance = this;
-        }
-        else
-        {
-            Destroy(gameObject);
-        }
-    }
+   
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Client"))
